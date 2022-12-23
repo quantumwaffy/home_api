210b7f93c83b2d7358a86c526c7be9be163333f6
quantumwaffy <sashchystyh@yandex.ru>
2022-12-23 15:56:15 +0300

    feat: env vars to react app

--- web/src/App.js
+++ web/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {process.env.REACT_APP_API_URL}
         </a>
       </header>
     </div>
